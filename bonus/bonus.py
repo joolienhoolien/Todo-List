59a3520fee5ed2e0c47b8c673a385ab1081394f5
joolienhoolien <julienpecquet@gmail.com>
2025-07-10 12:20:57 -0600

    Added "add" button.

--- bonus/bonus.py
+++
@@ -1,6 +0,0 @@
-password = input("Enter your password: ")
-
-while password != "pass123":
-    password = input("Enter your password: ")
-
-print("Password is correct!")
--- bonus/titleLength.py
+++
@@ -1,2 +0,0 @@
-user_input = input("Enter title here:")
-print("The length of the title", len(user_input))
